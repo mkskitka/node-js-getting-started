c2c2eb9208c76014b035790db7e617e0c4aaf5c0
Mary Skitka <mary.skitka@tufts.edu>
2016-04-02 18:48:34 -0400

    changes

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var cool = require('cool-ascii-faces');
+
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
+app.set('port', (process.env.PORT || 6000));
 app.use(express.static(__dirname + '/public'));
+app.listen(6000, function() { console.log('listening')});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// response.sendFile(_dirname + 'public/home.html')
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.sendFile('home.html');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
+// app.get('/lab8', function(request, response) {
+//   response.send(lab8());
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
