90b4375a194518dacb7318e2eeb775f4f8cda793
Mary Skitka <mary.skitka@tufts.edu>
2016-04-02 19:01:05 -0400

    commented out listener

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.listen(5000, function() { console.log('listening')});
+// app.listen(5000, function() { console.log('listening')});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
