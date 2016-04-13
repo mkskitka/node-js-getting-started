7d56aad5204c4e4aacb8fc024f053f4782f2f845
Mary Skitka <mary.skitka@tufts.edu>
2016-04-13 16:01:30 -0400

    getting started

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 // app.listen(5000, function() { console.log('listening')});
